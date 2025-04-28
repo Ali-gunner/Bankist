bfc5b4ffd68307128d16552a297d4521a1547c02
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-04-28 13:28:38 +0300

    Loan button functionality is added

--- script.js
+++ script.js
@@ -135,6 +135,19 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
